a657dfc105afc6cd1938291c83699b1099b77ab7
Yury Sauko <yury.savko@gmail.com>
2024-03-26 12:21:03 +0300

    Refactored the TaskFilter component from class to function

--- src/components/TasksFilter/TasksFilter.js
+++ src/components/TasksFilter/TasksFilter.js
@@ -1,61 +1,48 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 // import './TasksFilter.css';
 
-export default class TasksFilter extends Component {
-  static defaultProps = {
-    filter: 'All',
-  };
-
-  static propTypes = {
-    filter: PropTypes.string,
-    changeFilter: PropTypes.func.isRequired,
-  };
-
-  onClick = (e) => {
-    this.props.changeFilter(e.target.value);
-  };
-
-  render() {
-    const { filter } = this.props;
-
-    return (
-      <ul className="filters">
-        <li>
-          <button
-            value="All"
-            className={filter === 'All' ? 'selected' : ''}
-            type="button"
-            onClick={this.onClick}
-            // способ передачи объекта события внутри элемента
-            // onClick={(e) => {
-            //   this.props.changeFilter(e.target.value);
-            // }}
-          >
-            All
-          </button>
-        </li>
-        <li>
-          <button
-            value="Active"
-            className={filter === 'Active' ? 'selected' : ''}
-            type="button"
-            onClick={this.onClick}
-          >
-            Active
-          </button>
-        </li>
-        <li>
-          <button
-            value="Completed"
-            className={filter === 'Completed' ? 'selected' : ''}
-            type="button"
-            onClick={this.onClick}
-          >
-            Completed
-          </button>
-        </li>
-      </ul>
-    );
-  }
+export default function TasksFilter({ filter, changeFilter }) {
+  return (
+    <ul className="filters">
+      <li>
+        <button
+          value="All"
+          className={filter === 'All' ? 'selected' : ''}
+          type="button"
+          onClick={(e) => changeFilter(e.target.value)}
+        >
+          All
+        </button>
+      </li>
+      <li>
+        <button
+          value="Active"
+          className={filter === 'Active' ? 'selected' : ''}
+          type="button"
+          onClick={(e) => changeFilter(e.target.value)}
+        >
+          Active
+        </button>
+      </li>
+      <li>
+        <button
+          value="Completed"
+          className={filter === 'Completed' ? 'selected' : ''}
+          type="button"
+          onClick={(e) => changeFilter(e.target.value)}
+        >
+          Completed
+        </button>
+      </li>
+    </ul>
+  );
 }
+
+TasksFilter.defaultProps = {
+  filter: 'All',
+};
+
+TasksFilter.propTypes = {
+  filter: PropTypes.string,
+  changeFilter: PropTypes.func.isRequired,
+};
